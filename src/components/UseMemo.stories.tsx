ee3dfd38ee388b53931502700c1eb11ba8bef4b7
mazurenka <an.drej@mail.ru>
2022-01-22 13:34:13 +0100

    18 -  useMemo

--- src/components/UseMemo.stories.tsx
+++ src/components/UseMemo.stories.tsx
@@ -4,7 +4,7 @@ export default {
     title: "useMemo",
 }
 
-export const Example = () => {
+export const DifficultCountingExample = () => {
 
     const [a, setA] = useState<number>(5)
     const [b, setB] = useState<number>(5)
@@ -14,7 +14,7 @@ export const Example = () => {
 
     ResultA = useMemo(() => {
         let tempResultA = 1
-        for ( let i = 1; i <= a; i++) {
+        for (let i = 1; i <= a; i++) {
             let fake = 0;
             while (fake < 1000000) {
                 fake++;
@@ -26,10 +26,9 @@ export const Example = () => {
     }, [a]);
 
 
-    for ( let i = 1; i <= b; i++) {
+    for (let i = 1; i <= b; i++) {
         ResultB = ResultB * i;
     }
-
 
 
     return <>
@@ -47,6 +46,39 @@ export const Example = () => {
         <div>
             Result for b: {ResultB}
         </div>
+    </>
+}
+
+const UsersSecret = (props: { users: Array<string> }) => {
+    debugger
+    console.log('USERS')
+    return <div>{
+        props.users.map((u, i) => <div key={i}>{u}</div>)
+    }</div>
+}
+
+const Users = React.memo(UsersSecret)
+
+export const HelpsToReactMemo = () => {
+    console.log('HelpsToReactMemo')
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Dima', "Valera", "Artem"])
+
+    const newArray = useMemo(() => {
+        const newArray = users.filter(u => u.toLowerCase().indexOf('a') > -1)
+        return newArray
+    }, [users])
+
+    const addUser = () => {
+        const newUsers = [...users, 'Sveta ' + new Date().getTime()]
+        setUsers(newUsers)
+    }
+
+    return <>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={() => addUser()}>add user</button>
+        {counter}
+        <Users users={newArray}/>
     </>
 }
 
