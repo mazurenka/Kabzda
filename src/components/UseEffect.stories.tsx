9ad618d4f5d65ca1e1ce0632af0d1978d5f51fe3
mazurenka <an.drej@mail.ru>
2022-01-24 10:57:32 +0100

    21 - useEffect основы

--- src/components/UseEffect.stories.tsx
+++ src/components/UseEffect.stories.tsx
@@ -5,22 +5,30 @@ export default {
 }
 
 export const SimpleExample = () => {
+    const [fake, setFake] = useState(1)
     const [counter, setCounter] = useState(1)
 
     console.log('SimpleExample')
 
     useEffect(() => {
-        console.log("UseEffect")
-        //api.getUsers().then("")
-        //setInterval
-        //indexedDB
-        //document.getElementById
-        //document.title = "User"
-    })
+        console.log("UseEffect every render")
+        document.title = counter.toString();
+    },)
+
+    useEffect(() => {
+        console.log("UseEffect only first render")
+        document.title = counter.toString();
+    }, [])
+
+    useEffect(() => {
+        console.log("UseEffect first render and every change counter")
+        document.title = counter.toString();
+    }, [counter])
 
     return <>
-        Hello, {counter}
+        Hello, {counter} {fake}
         <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={() => setFake(fake + 1)}>+</button>
     </>
 }
 
