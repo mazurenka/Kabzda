78abbbcf0a8f5818612e1640723f6f704e5042cd
mazurenka <an.drej@mail.ru>
2022-01-24 15:58:33 +0100

    23 - useEffect - компонента часы

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from "react";
 
 type PropsType = {}
 
+const get2DigetsString = (num: number) => num < 10 ? "0" + num : num
+
 export const Clock: React.FC<PropsType> = (props) => {
 
     const [date, setDate] = useState(new Date())
@@ -9,20 +11,20 @@ export const Clock: React.FC<PropsType> = (props) => {
     useEffect(() => {
         setInterval(() => {
             setDate(new Date())
-        })
+        }, 1000)
     }, [])
 
     return <div>
-        <span>{date.getFullYear()}</span>
+        <span>{get2DigetsString(date.getFullYear())}</span>
         :
-        <span>{date.getMonth()}</span>
+        <span>{get2DigetsString(date.getMonth())}</span>
         :
-        <span>{date.getDay()}</span>
+        <span>{get2DigetsString(date.getDay())}</span>
         :
-        <span>{date.getHours()}</span>
+        <span>{get2DigetsString(date.getHours())}</span>
         :
-        <span>{date.getMinutes()}</span>
+        <span>{get2DigetsString(date.getMinutes())}</span>
         :
-        <span>{date.getSeconds()}</span>
+        <span>{get2DigetsString(date.getSeconds()}</span>
     </div>
 }
